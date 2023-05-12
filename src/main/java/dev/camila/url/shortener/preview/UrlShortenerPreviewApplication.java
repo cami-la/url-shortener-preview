4d56713b8593e8245ccc0c88f6b4ca5fd64af265
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2023-05-12 17:59:36 -0300

    update

--- src/main/java/dev/camila/url/shortener/preview/UrlShortenerPreviewApplication.java
+++ src/main/java/dev/camila/url/shortener/preview/UrlShortenerPreviewApplication.java
@@ -5,13 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UrlShortenerPreviewApplication implements CommandLineRunner {
+public class UrlShortenerPreviewApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UrlShortenerPreviewApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-	}
 }
